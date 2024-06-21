0296384da84ff05795339ed2551b02597dc3532c
aleksander <147951270+stanuch@users.noreply.github.com>
2024-06-21 20:26:51 +0200

    Delete cwiczenia8 directory

--- cwiczenia8/zadanie2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-txt = "kot 123-435-265 kanarek +48-654-235-542 rybki 321 432 543"
-
-pattern = "\+48-[0-9]{3}-[0-9]{3}-[0-9]{3}|[0-9]{3}[ -][0-9]{3}[ -][0-9]{3}"
-
-result = re.findall(pattern, txt)
-
-print(result)
--- cwiczenia8/zadanie4.py
+++
@@ -1,8 +0,0 @@
-import re
-
-txt = "https://pl.wikipedia.org/wiki/Wyra%C5%BCenie_regularne kot pies kanarek https://pl.wikipedia.org/wiki/Gramatyka_regularna rybki"
-
-pattern = "https*://[^ ]*"
-result = re.findall(pattern, txt)
-
-print(result)
