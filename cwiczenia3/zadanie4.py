a5916faeab6a8d0663c0f434814c8564122cde74
aleksander <147951270+stanuch@users.noreply.github.com>
2024-06-21 20:28:42 +0200

    Delete cwiczenia3 directory

--- cwiczenia3/zadanie4.py
+++
@@ -1,8 +0,0 @@
-#Napisz listę składaną przypisująca wartość func_a(x) dla x parzystych i func_b(x) dla x nieparzystych. 0 < x < 10
-
-func_a = lambda x: x * 3.14
-func_b = lambda x: x / 17
-
-list = [func_a(liczba) if liczba % 2 == 0 else func_b(liczba) for liczba in range(1,10)]
-
-print(list)
