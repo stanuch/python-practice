c5425c63c733b5c266f5ffa86fc1d9ea7ba01310
aleksander <147951270+stanuch@users.noreply.github.com>
2024-06-21 20:28:47 +0200

    Delete cwiczenia2 directory

--- cwiczenia2/zadanie1.py
+++
@@ -1,8 +0,0 @@
-def wielomian(x):
-    return 3*x**3 - 2*x**2 + 7*x - 12
-
-x = int(input(f"Podaj liczbę 'x': "))
-
-wynik_wielomianu = wielomian(x)
-
-print(f"Wynik wielomianu dla x = {x}: {wynik_wielomianu}")
