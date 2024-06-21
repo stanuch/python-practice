e361082e87c1519a0897ba430fae1fa19b58d904
aleksander <147951270+stanuch@users.noreply.github.com>
2024-06-21 20:28:52 +0200

    Delete cwiczenia1 directory

--- cwiczenia1/zadanie1.py
+++
@@ -1,9 +0,0 @@
-data = [34, -3.24, 98, 43, "zimorodek", None, 121, -73, 6.51, 4 + 7j, 2, 87, 32, 9.32, -30]
-
-suma = 0
-
-for element in data:
-    if type(element) == int:
-        suma += element
-
-print(suma)
